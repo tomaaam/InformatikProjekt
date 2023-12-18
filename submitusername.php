e3e677abfe3c5d1c437c8fe2bedb1755fedaaca2
Tom <98837941+tomaaam@users.noreply.github.com>
2023-12-18 20:37:15 +0100

    Update submitusername.php

--- submitusername.php
+++ submitusername.php
@@ -8,6 +8,12 @@
         $username = mysqli_real_escape_string($db_link, $username);
         $score = mysqli_real_escape_string($db_link, $score);
 
+        if (empty($username)) {
+            runSQL("INSERT INTO s1 (USERNAME, SCORE) VALUES ('" . "Anonym" . "' , '" . $score . "')");
+        }
+
+        else {
         runSQL("INSERT INTO s1 (USERNAME, SCORE) VALUES ('" . $username . "' , '" . $score . "')");
+        }    
     }
 ?>
