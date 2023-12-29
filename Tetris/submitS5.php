e524357c15407aadbe9aad18be2848706eb63ece
Tom <98837941+tomaaam@users.noreply.github.com>
2023-12-29 23:05:04 +0100

    Add files via upload

--- Tetris/submitS5.php
+++ Tetris/submitS5.php
@@ -1,1 +1,19 @@
+<?php
 
+    function register($username, $score) {
+        
+        echo('<p>Erfolgreich!</p>');
+            
+        global $db_link;
+        $username = mysqli_real_escape_string($db_link, $username);
+        $score = mysqli_real_escape_string($db_link, $score);
+
+        if (empty($username)) {
+            runSQL("INSERT INTO s5 (USERNAME, SCORE) VALUES ('" . "Anonym" . "' , '" . $score . "')");
+        }
+
+        else {
+        runSQL("INSERT INTO s5 (USERNAME, SCORE) VALUES ('" . $username . "' , '" . $score . "')");
+        }    
+    }
+?>
