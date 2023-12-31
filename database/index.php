d39c6e463b00ae20ed15a8131a5db051b04f4649
Tom <98837941+tomaaam@users.noreply.github.com>
2023-12-31 22:25:55 +0100

    Update index.php

--- database/index.php
+++ database/index.php
@@ -4,33 +4,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testumgebung</title>
     <script>
-      function generateTestscore() { //Generierung eines beispielhaften Highscores, ersetzt eure Definition eines "echten" Highscores zu Testzwecken
+      function generateTestscore() {
         var score = Math.floor(Math.random() * 10000);
         document.getElementById("highscore").innerHTML = score;
-        document.getElementById("hiddenScore").value = score; //Der Highscore wird in das "versteckte" Objekt geschrieben  
+        document.getElementById("hiddenScore").value = score;
       }
     </script>
 </head>
 <body>
 
-  <p>Highscore: <span id="highscore"></span></p>
+  Highscore: <span id="highscore"></span><br />
   <button onclick=generateTestscore()>Highscore generieren</button>
+  <p>
 
   <?php
-    require('submitS1.php'); //HIER MUSS "S1" durch euren Teil ersetzt werden! (siehe "README.md")
-    require('connector.php'); //Hier muss nichts geändert werden
+    require('submitS1.php');
+    require('connector.php'); 
 
-    if(isset($_POST['submit'])) { 
+    if(isset($_POST['submit'])) {
 
-      register($_POST['username'], $_POST['score']); //Sendung von Daten an die Datenbank über die jeweilige "submitS[].php"-Datei
+      register($_POST['username'], $_POST['score']);
 
     }
       
     if (isset($_POST['auslesen'])) {
         
-      $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM S1 ORDER BY SCORE DESC"); //Empfang von Daten aus der Datenbank
+      $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM s1 ORDER BY SCORE DESC");
       
-      echo('<table>'); //Ausgabe der Daten als unformatierte Tabelle
+      echo('<table>');
       while($row = mysqli_fetch_array($db_res)) {
         echo('<tr>');
         echo('<td>' . $row['USERNAME'] . '</td>');
@@ -40,14 +41,55 @@
       }
       echo('</table>');
     }
+
+    if (isset($_POST['lookup'])) {
+
+      $search = isset($_POST['search']) ? $_POST['search'] : '';
+
+      global $db_link;
+      $search = mysqli_real_escape_string($db_link, $search);
+
+      if (empty($search)) {
+        echo '<script type="text/javascript">alert("Zuerst einen Nutzernamen eingeben!");</script>';
+      }
+
+      else {
+        $result = runSQL("SELECT COUNT(*) as count FROM S1 WHERE USERNAME = '$search'");
+        $row = mysqli_fetch_assoc($result);
+
+        $count = intval($row['count']);
+
+        if ($count > 0) {
+          $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM S1 WHERE USERNAME = '$search';");
+
+          echo('<table>');
+          while($row = mysqli_fetch_array($db_res)) {
+            echo('<tr>');
+            echo('<td>' . $row['USERNAME'] . '</td>');
+            echo('<td>' . $row['SCORE'] . '</td>');
+            echo('<td>' . $row['DATE'] . '</td>');
+            echo('</tr>');
+          }
+          echo('</table>');
+        }
+
+        else {
+          echo '<script type="text/javascript">alert("Dieser Nutzername existiert nicht!");</script>';
+        }
+      }
+    }
   ?>
 
   <form action="index.php" method="POST">
     <label>Nutzernamen eingeben:</label>
-    <input type="text" name="username" /> <br /> <!-- Eingabe des Usernamens -->
-    <input type="hidden" name="score" id="hiddenScore" /> <!-- Hier wird der Highscore hereingeschrieben, damit die "register"-Funktion auf auf ihn zugreifen kann -->
-    <input type="submit" name="submit" value="Bestätigen" /> 
-    <input type="submit" name="auslesen" value="Tabelle anzeigen" /> 
+    <input type="text" name="username" /> <br />
+    <input type="hidden" name="score" id="hiddenScore" />
+    <input type="submit" name="submit" value="Bestätigen" />
+    <input type="submit" name="auslesen" value="Tabelle anzeigen" /> <br />
+    <p>
+    <label>Nach Nutzernamen suchen:</label>
+    <input type="text" name="search" /> <br />
+    <input type="submit" name="lookup" value="Nach Score suchen" /> <br />
   </form>
     
 </body>
