5517dcda160154c27f441b4140bc5893d38ec8fe
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-03 23:35:50 +0100

    Update index.php

--- 2048/index.php
+++ 2048/index.php
@@ -1,3 +1,67 @@
+<?php
+    require('submitS4.php');
+    require('../connector.php'); 
+
+    if(isset($_POST['submit'])) {
+      register($_POST['username'], $_POST['score']);
+    }
+      
+    if (isset($_POST['auslesen'])) {
+      // Display top scores
+      displayTopScores();
+    }
+
+    if (isset($_POST['lookup'])) {
+      $search = isset($_POST['search']) ? $_POST['search'] : '';
+
+      global $db_link;
+      $search = mysqli_real_escape_string($db_link, $search);
+
+      if (empty($search)) {
+        echo '<script type="text/javascript">alert("Zuerst einen Nutzernamen eingeben!");</script>';
+      } else {
+        searchUserScores($search);
+      }
+    }
+
+    function displayTopScores() {
+      $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM s5 ORDER BY SCORE DESC");
+      
+      echo('<table>');
+      while($row = mysqli_fetch_array($db_res)) {
+        echo('<tr>');
+        echo('<td>' . $row['USERNAME'] . '</td>');
+        echo('<td>' . $row['SCORE'] . '</td>');
+        echo('<td>' . $row['DATE'] . '</td>');
+        echo('</tr>');
+      }
+      echo('</table>');
+    }
+
+    function searchUserScores($username) {
+      global $db_link;
+
+      $result = runSQL("SELECT COUNT(*) as count FROM s5 WHERE USERNAME = '$username'");
+      $row = mysqli_fetch_assoc($result);
+      $count = intval($row['count']);
+
+      if ($count > 0) {
+        $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM s5 WHERE USERNAME = '$username' ORDER BY SCORE DESC;");
+
+        echo('<table>');
+        while($row = mysqli_fetch_array($db_res)) {
+          echo('<tr>');
+          echo('<td>' . $row['USERNAME'] . '</td>');
+          echo('<td>' . $row['SCORE'] . '</td>');
+          echo('<td>' . $row['DATE'] . '</td>');
+          echo('</tr>');
+        }
+        echo('</table>');
+      } else {
+        echo '<script type="text/javascript">alert("Dieser Nutzername existiert nicht!");</script>';
+      }
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
