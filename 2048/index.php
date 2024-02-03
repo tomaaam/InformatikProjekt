76f081dd96752844f9c78dc9b581b191daf29914
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-04 00:43:47 +0100

    Update index.php

--- 2048/index.php
+++ 2048/index.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>2048</title>
     <link rel="stylesheet" href="2048.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="2048.js"></script>
 </head>
 
@@ -20,45 +19,7 @@
     <div id="endScreen" style="display: none;">
         <h3>Game Over!</h3>
         <p>Your score: <span id="finalScore">0</span></p>
-        <div id="tableContainer"></div> <!-- Container for the table -->
-        <form action="index.php" method="POST">
-            <label>Nutzernamen eingeben:</label>
-            <input type="text" name="username" /> <br />
-            <input type="hidden" name="score" id="hiddenScore" />
-            <input type="submit" name="submit" value="Bestätigen" />
-            <input type="button" id="showTable" value="Tabelle anzeigen" /> <br />
-            <p>
-            <label>Nach Nutzernamen suchen:</label>
-            <input type="text" name="search" /> <br />
-            <input type="submit" name="lookup" value="Nach Score suchen" /> <br />
-        </form>
     </div>
 </body>
-
-<script>
-$('form').submit(function(event) {
-    event.preventDefault(); // Prevent the form from being submitted the traditional way
-
-    var username = $('input[name="username"]').val(); // Get the username from the input field
-    var score = $('#score').text(); // Corrected to use the correct ID
-
-    $.ajax({
-        url: 'submitS4.php', // The URL of the PHP file that handles the data
-        type: 'POST', // The HTTP method to use
-        data: { // The data to send to the server
-            'username': username,
-            'score': score
-        },
-        success: function(data) {
-            console.log('Success:', data);
-            $('#tableContainer').html(data);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log('Error:', textStatus, errorThrown);
-        }
-    });
-});
-
-</script>
 
 </html>
