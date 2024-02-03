be16decaf1a700ecfe65e9e6fb6e94cb3d92e56d
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-03 13:43:35 +0100

    Update getTopScores.php

--- blockjumpinggame/getTopScores.php
+++ blockjumpinggame/getTopScores.php
@@ -1,6 +1,7 @@
+<?php
 // getTopScores.php
 
-// ... (existing code)
+require('connector.php');
 
 function getTopScores() {
     global $db_link;
@@ -9,7 +10,6 @@ function getTopScores() {
         die(json_encode(['status' => 'error', 'message' => 'Connection failed: ' . mysqli_connect_error()]));
     }
 
-    // Change LIMIT to 10 to fetch the top 10 scores
     $query = "SELECT * FROM s1 ORDER BY SCORE DESC LIMIT 10";
     $result = runSQL($query);
 
@@ -26,3 +26,4 @@ function getTopScores() {
 }
 
 getTopScores();
+?>
