f7ab283f9d8beaf376c7c0613a0d195f27450c33
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-03 22:50:13 +0100

    Update index.php

--- tetris/index.php
+++ tetris/index.php
@@ -520,6 +520,12 @@
         }
 
         init();
+if (punktestand.isSpielVorbei()) {
+    document.getElementById('hiddenScore').value = punktestand.getPunkte();
+    // Submit the form
+    document.forms[0].submit();
+    return;
+}
 </script>
 <?php
     require('submitS5.php');
