cff03d43c552f094a1fac6492557316f977a4e95
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-03 23:51:18 +0100

    Update index.php

--- 2048/index.php
+++ 2048/index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>2048</title>
-        <link rel="stylesheet" href="2048.css">
-        <script src="2048.js"></script>
-    </head>
 
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2048</title>
+    <link rel="stylesheet" href="2048.css">
+    <script src="2048.js"></script>
+</head>
+
+<body>
     <h1>2048</h1>
     <hr>
     <h2>Score: <span id="score">0</span></h2>
@@ -18,6 +19,64 @@
     <div id="endScreen" style="display: none;">
         <h3>Game Over!</h3>
         <p>Your score: <span id="finalScore">0</span></p>
+
+        <!-- Integrate the PHP code here -->
+        <?php
+        require('submitS1.php');
+        require('connector.php');
+
+        if (isset($_POST['submit'])) {
+            register($_POST['username'], $_POST['score']);
+        }
+
+        if (isset($_POST['auslesen'])) {
+            $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM s1 ORDER BY SCORE DESC");
+
+            echo('<table>');
+            while ($row = mysqli_fetch_array($db_res)) {
+                echo('<tr>');
+                echo('<td>' . $row['USERNAME'] . '</td>');
+                echo('<td>' . $row['SCORE'] . '</td>');
+                echo('<td>' . $row['DATE'] . '</td>');
+                echo('</tr>');
+            }
+            echo('</table>');
+        }
+
+        if (isset($_POST['lookup'])) {
+            $search = isset($_POST['search']) ? $_POST['search'] : '';
+
+            global $db_link;
+            $search = mysqli_real_escape_string($db_link, $search);
+
+            if (empty($search)) {
+                echo '<script type="text/javascript">alert("Zuerst einen Nutzernamen eingeben!");</script>';
+            } else {
+                $result = runSQL("SELECT COUNT(*) as count FROM S1 WHERE USERNAME = '$search'");
+                $row = mysqli_fetch_assoc($result);
+
+                $count = intval($row['count']);
+
+                if ($count > 0) {
+                    $db_res = runSQL("SELECT USERNAME, SCORE, DATE FROM S1 WHERE USERNAME = '$search' ORDER BY SCORE DESC;");
+
+                    echo('<table>');
+                    while ($row = mysqli_fetch_array($db_res)) {
+                        echo('<tr>');
+                        echo('<td>' . $row['USERNAME'] . '</td>');
+                        echo('<td>' . $row['SCORE'] . '</td>');
+                        echo('<td>' . $row['DATE'] . '</td>');
+                        echo('</tr>');
+                    }
+                    echo('</table>');
+                } else {
+                    echo '<script type="text/javascript">alert("Dieser Nutzername existiert nicht!");</script>';
+                }
+            }
+        }
+        ?>
+        <!-- End of integrated PHP code -->
+
         <form action="index.php" method="POST">
             <label>Enter your username:</label>
             <input type="text" name="username" />
@@ -26,4 +85,5 @@
         </form>
     </div>
 </body>
+
 </html>
