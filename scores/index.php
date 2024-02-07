1f09d96012abcc10de7b5f6a8868b78f3b4cfd8f
NiklasRichter2222 <88104798+NiklasRichter2222@users.noreply.github.com>
2024-02-07 18:22:39 +0100

    Update index.php

--- scores/index.php
+++ scores/index.php
@@ -46,7 +46,7 @@ if(isset($_GET['game'])) {
             margin: 0 auto;
         }
         th, td {
-            border: 1px solid #888;
+            border: 1px solid #aaa;
             padding: 8px;
             text-align: left;
         }
